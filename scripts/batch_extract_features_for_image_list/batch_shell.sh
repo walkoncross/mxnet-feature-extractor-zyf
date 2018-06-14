9bca1d28537593411f8a64de0e702f2ebfe5481e
root <810315118@qq.com>
2018-06-14 13:11:13 +0800

    split filelist201806141310

--- scripts/batch_extract_features_for_image_list/batch_shell.sh
+++ scripts/batch_extract_features_for_image_list/batch_shell.sh
@@ -3,6 +3,7 @@
 export PYTHONPATH=/usr/local/lib/python2.7/dist-packages:$PYTHON
 export MXNET_CUDNN_AUTOTUNE_DEFAULT=0
 
+GPU_NUM=8
 MODEL_ROOT="/workspace/data/insightface-models/"
 #change your model name
 MODEL_NAME="model-r100-spa-m2.0/slim/model-r100-slim,96"
@@ -12,6 +13,26 @@ echo $MODEL_PATH
 SAVE_ROOT="/workspace/data/shiyongjie/data/"
 SAVE_NAME="megaface_challenge2_features"
 SAVE_DIR="${SAVE_ROOT}${SAVE_NAME}"
+
+#split feature file list
+FEATURE_FILE_LIST="/workspace/data/shiyongjie/code/megaface2_filelist.txt"
+SPLIT_FILE_LIST_SAVE_PATH="./filelist"
+SPLIT_FILE_LIST_PREFIX="split"
+DIVISION_NUM=$GPU_NUM
+
+if [ ! -d $SPLIT_FILE_LIST_SAVE_PATH ];then
+   mkdir $SPLIT_FILE_LIST_SAVE_PATH
+fi
+
+python split.py \
+        --division=$DIVISION_NUM \
+        --save-dir=$SPLIT_FILE_LIST_SAVE_PATH \
+	--file-list=$FEATURE_FILE_LIST  \
+	--file-prefix=$SPLIT_FILE_LIST_PREFIX  \
+        &
+       
+wait
+
 
 if [ ! -d $SAVE_DIR ];then
    mkdir SAVE_DIR
@@ -26,7 +47,6 @@ fi
 LOG_NAME="./logs/nohup-extract-log-megafce-challenge2"
 imagelistprefix="/workspace/data/shiyongjie/code/batch_feature_extract/filelist/megaface2_aligned_face_filelist-split"
 image_dir="/workspace/data/shiyongjie/code/tron_refinedet_gpu/megaface_aligned_face/aligned_imgs"
-GPU_NUM=8
 
 for ((i=0; i<$GPU_NUM; i++))
     do
