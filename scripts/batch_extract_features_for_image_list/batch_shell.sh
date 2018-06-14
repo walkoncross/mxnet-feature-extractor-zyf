f481f4949fca20a67d3c232db359388b85d161ea
root <810315118@qq.com>
2018-06-14 09:19:39 +0800

    modify201806140930

--- scripts/batch_extract_features_for_image_list/batch_shell.sh
+++ scripts/batch_extract_features_for_image_list/batch_shell.sh
@@ -6,35 +6,41 @@ export MXNET_CUDNN_AUTOTUNE_DEFAULT=0
 MODEL_ROOT="/workspace/data/insightface-models/"
 #change your model name
 MODEL_NAME="model-r100-spa-m2.0/slim/model-r100-slim,96"
-# MODEL_PATH="${MODEL_ROOT}${MODEL_NAME}"
-echo $MODEL_NAME
+MODEL_PATH="${MODEL_ROOT}${MODEL_NAME}"
+echo $MODEL_PATH
 #feature save
-SAVE_ROOT="/workspace/data/shiyongjie/model/"
-SAVE_NAME="model-r100-spa-m2.5-8gpu-ep32"
+SAVE_ROOT="/workspace/data/shiyongjie/data/"
+SAVE_NAME="megaface_challenge2_features"
 SAVE_DIR="${SAVE_ROOT}${SAVE_NAME}"
+
+if [ ! -d $SAVE_DIR ];then
+   mkdir SAVE_DIR
+fi
+
 
 #log
 logPath="./logs"
-if [ ! -d "$logPath"];then
-   mkdir  "$logPath"
+if [ ! -d $logPath ];then
+   mkdir  $logPath
 fi
-
-LOG_NAME="./logs/nohup-extract-log-ms1m-vggface2-model-r100-spa-m2.5-8gpu-ep32"
-imagelistprefix="/workspace/data/shiyongjie/project/tron/megaface-challenge2-aligned-face-filelist-split"
+LOG_NAME="./logs/nohup-extract-log-megafce-challenge2"
+imagelistprefix="/workspace/data/shiyongjie/code/batch_feature_extract/filelist/megaface2_aligned_face_filelist-split"
+image_dir="/workspace/data/shiyongjie/code/tron_refinedet_gpu/megaface_aligned_face/aligned_imgs"
 GPU_NUM=8
 
 for ((i=0; i<$GPU_NUM; i++))
     do
         /bin/sleep 2
         nohup python extract_features_for_aligned_imagelist.py \
-              --model=$MODEL_NAME \
+              --model=$MODEL_PATH \
               --image-list=$imagelistprefix${i}.txt \
-              --image-dir=/workspace/data/qyc \
+              --image-dir=$image_dir  \
               --save-dir=$SAVE_DIR \
-              --batch-size=50 \
+              --batch-size=20 \
               --image-size=3,112,112 \
               --add-flip \
               --gpu=$i \
               --save-format=.npy \
-              --flip-sim > ${LOG_NAME}"split"${i}".txt" 2>&1 &
+              --flip-sim > ${LOG_NAME}"_split_"${i}".txt" 2>&1 &
+      echo "i="$i
     done
